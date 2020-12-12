a33be684675f7fe6734e09523d1a954d8dbe35f4
ArvidHast <hasta@student.chalmers.se>
2020-12-12 23:58:02 +0100

    Solve 4

---
+++ 4/solve.py
@@ -0,0 +1,31 @@
+import hashlib
+filepath = 'data.txt'
+
+
+l = []
+
+with open(filepath) as fp:
+    line = fp.readline()
+    while line:
+        l.append(line.strip())
+        line = fp.readline()
+
+nl = len(l)
+
+c = 0
+
+n = 0
+
+for i in range(nl):
+    tl = l[i]
+
+    res = hashlib.md5((tl + str(n)).encode('utf-8')).hexdigest()
+
+    while res[:5] != "00000":
+        res = hashlib.md5((tl + str(n)).encode('utf-8')).hexdigest()
+        n += 1
+
+    print("md5: ", n-1)
+
+
+print("answer: ", c)
